ba3a57833b320ea732a4deb5d7146731eeaefccb
trocatudo95 <pedromachado_95@sapo.pt>
2019-04-18 10:13:45 +0100

    minor fix

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
+
+
 async function scenario1() {   
+    console.log("gey")
     let queue = new AsyncQueue()
     let p1 = new Publisher()
     let s1 = new Subscriber('subscriber_1')
     s1.pull(queue)
     p1.push(queue, 'ola')
 }
+
+scenario1();
